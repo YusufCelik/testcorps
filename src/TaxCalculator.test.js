464b93948c7ab48a57c832297a4ba97f297864b0
Yusuf Celik <ycelik@Yusufs-MacBook-Pro.local>
2019-04-06 16:34:16 +0200

    Added excersize 6 test

--- src/TaxCalculator.test.js
+++ src/TaxCalculator.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import TaxCalculator from "./TaxCalculator";
 import Button from "./Button";
@@ -7,7 +7,7 @@ import Button from "./Button";
 describe("<TaxCalculator />", () => {
   it("renders calculate button", () => {
     const wrapper = shallow(<TaxCalculator />);
-    expect(wrapper.find(Button).exists()).toBe(true);
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("renders tax total label", () => {
@@ -15,5 +15,28 @@ describe("<TaxCalculator />", () => {
     expect(
       wrapper.find('[data-testhook-id="tax-calculator-total"]').exists()
     ).toBe(true);
+  });
+
+  it("changes tax total when user clicks calculate", () => {
+    const wrapper = mount(<TaxCalculator />);
+    wrapper
+      .find('[data-testhook-id="tax-calculator-revenue-input"]')
+      .instance().value = "20";
+
+    wrapper
+      .find('[data-testhook-id="tax-calculator-revenue-input"]')
+      .simulate("change");
+
+    wrapper.find(Button).simulate("click");
+    expect(
+      wrapper.find('[data-testhook-id="tax-calculator-total"]').text()
+    ).toEqual("20");
+
+    expect(wrapper.instance().state).toEqual({ total: "20" });
+    wrapper.setState({ total: "40" });
+
+    expect(
+      wrapper.find('[data-testhook-id="tax-calculator-total"]').text()
+    ).toEqual("40");
   });
 });
