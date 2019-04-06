48084695a277dcc4c48ce5bdd8339cbe748bbcbe
Yusuf Celik <ycelik@Yusufs-MacBook-Pro.local>
2019-04-06 13:26:23 +0200

    New base test file

--- src/TaxCalculator.test.js
+++ src/TaxCalculator.test.js
@@ -9,4 +9,11 @@ describe("<TaxCalculator />", () => {
     const wrapper = shallow(<TaxCalculator />);
     expect(wrapper.find(Button).exists()).toBe(true);
   });
+
+  it("renders tax total label", () => {
+    const wrapper = shallow(<TaxCalculator />);
+    expect(
+      wrapper.find('[data-testhook-id="tax-calculator-total"]').exists()
+    ).toBe(true);
+  });
 });
